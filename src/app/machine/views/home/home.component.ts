374c9604e1d98dd3e01e4c76570534f0fabda5c1
joseDev29 <jose.1701911606@ucaldas.edu.co>
2022-06-18 01:40:23 -0500

    feat: style improvements

--- src/app/machine/views/home/home.component.ts
+++ src/app/machine/views/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { MessageService } from 'primeng/api';
 import { MachineStateService } from '../../machine-state.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css'],
 })
 export class HomeComponent {
   inputValue: string = '';
